6927679fc1676f2d9853f5208566e492f564236f
magnusouren <magnut0203@gmail.com>
2023-09-05 19:12:45 +0200

    Wrappet appen i QueryClientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+
+const queryClient = new QueryClient();
 
 import { Router } from './routes';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={Router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={Router} />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
