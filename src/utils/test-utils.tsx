d8501c25db3e05d1dede410040fc12e9c7084200
magnusouren <magnut0203@gmail.com>
2023-09-06 11:45:35 +0200

    comment on function

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 
+// Fucntion to render a component wrapped inside a browserrouter for testing purposes
 export const renderWithRouter = (component: JSX.Element) => render(<BrowserRouter>{component}</BrowserRouter>);
