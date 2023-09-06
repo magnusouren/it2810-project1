d7c5a62866b1eafbe1feb0cee8d691d49e2218ca
magnusouren <magnut0203@gmail.com>
2023-09-06 11:44:31 +0200

    add pages for correct routing

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,18 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 
 import { PageLayout } from './layouts/PageLayout.tsx';
+import { Favorites } from './pages/Favorites.tsx';
 import { Home } from './pages/Home.tsx';
 import { NotFound } from './pages/NotFound.tsx';
+import { Search } from './pages/Search.tsx';
 
 export const Router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path='/' element={<PageLayout />}>
         <Route path='/' element={<Home />} />
-        <Route path='/test' element={<p>Test</p>} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/favorites' element={<Favorites />} />
       </Route>
       <Route path='*' element={<NotFound />} />
     </>,
