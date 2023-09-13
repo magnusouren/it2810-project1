b97018f9fbf11fa1ef5e79ca94b8caaba36bf928
Jakob Relling <jakobere@stud.ntnu.no>
2023-09-13 19:06:38 +0200

    nedtrekksliste for filtrering

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,13 +1,52 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { FC } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import { CategoryType } from '../types';
 
-export const Search = () => {
+interface SearchProps {
+  searchCategory?: CategoryType;
+}
+
+export const Search: FC<SearchProps> = ({ searchCategory }) => {
   const { state } = useLocation();
 
+  const { data, isLoading, isSuccess } = useQuery<CategoryType[]>(['categories'], () =>
+    axios
+      .get('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list')
+      .then((res) => res.data.drinks.map((drink: { strCategory: string }) => drink.strCategory)),
+  );
+
+  // const changeCategory = (e) => {
+
+  // };
+  console.log(state);
+  console.log(searchCategory);
+  if (isLoading) return <div>Loading...</div>;
   !state && window.location.replace('/');
 
   const category = state as CategoryType;
 
-  return <>{category}</>;
+  return (
+    <>
+      <div>
+        {data && isSuccess && (
+          <select
+            placeholder='Filter by category'
+            // onChange={(e) => {
+            //   changeCategory(e);
+            // }}
+          >
+            {data.sort().map((category: CategoryType) => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        )}
+        {category}
+      </div>
+    </>
+  );
 };
