28dd95629d26c76d7d00dfe5e82a1b36464e080a
Jakob Relling <jakobere@stud.ntnu.no>
2023-09-13 20:04:23 +0200

    api call on category

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import { CategoryType } from '../types';
@@ -9,8 +9,13 @@ interface SearchProps {
   searchCategory?: CategoryType;
 }
 
+interface Cat {
+  name: string;
+}
+
 export const Search: FC<SearchProps> = ({ searchCategory }) => {
   const { state } = useLocation();
+  const [Cat, setCat] = useState<Cat | null>(null);
 
   const { data, isLoading, isSuccess } = useQuery<CategoryType[]>(['categories'], () =>
     axios
@@ -18,9 +23,15 @@ export const Search: FC<SearchProps> = ({ searchCategory }) => {
       .then((res) => res.data.drinks.map((drink: { strCategory: string }) => drink.strCategory)),
   );
 
-  // const changeCategory = (e) => {
-
-  // };
+  useEffect(() => {
+    if (searchCategory) {
+      axios
+        .get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${searchCategory}`)
+        .then((res) => console.log(res));
+    } else if (Cat) {
+      axios.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${Cat.name}`).then((res) => console.log(res));
+    }
+  }, [Cat, searchCategory]);
   console.log(state);
   console.log(searchCategory);
   if (isLoading) return <div>Loading...</div>;
@@ -34,9 +45,9 @@ export const Search: FC<SearchProps> = ({ searchCategory }) => {
         {data && isSuccess && (
           <select
             placeholder='Filter by category'
-            // onChange={(e) => {
-            //   changeCategory(e);
-            // }}
+            onChange={(e) => {
+              setCat({ name: e.target.value });
+            }}
           >
             {data.sort().map((category: CategoryType) => (
               <option key={category} value={category}>
