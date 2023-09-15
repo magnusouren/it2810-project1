757b9d9cd9f7098be6271cbdf4e64abaf63514b9
magnusouren <magnut0203@gmail.com>
2023-09-15 14:13:55 +0200

    mocking tests for categories

--- src/components/categories/__tests__/Categories.test.tsx
+++ src/components/categories/__tests__/Categories.test.tsx
@@ -1,57 +1,56 @@
 import { screen, waitFor } from '@testing-library/react';
 import nock from 'nock';
 
+import { CategoryType } from '../../../types';
 import { renderWithRouterAndQueryClient } from '../../../utils/test-utils';
 import { Categories } from '../Categories';
 
-// vi.mock('@tanstack/react-query', async () => {
-//   const axios = (await vi.importActual('@tanstack/react-query')) as { default?: undefined };
-//   return {
-//     ...axios,
-//     useQuery: () => ({
-//       data: ['Beer', 'Cocoa', 'Coffee'] as CategoryType[],
-//       isLoading: false,
-//       isSuccess: true,
-//     }),
-//   };
-// });
-
 const mockApiReturn = [
   {
-    strCategory: 'Beer',
+    strCategory: 'Beer' as CategoryType,
   },
   {
-    strCategory: 'Cocoa',
+    strCategory: 'Cocoa' as CategoryType,
   },
   {
-    strCategory: 'Coffee',
+    strCategory: 'Coffee / Tea' as CategoryType,
   },
 ];
 
 describe('Categories', () => {
-  it('Should match snapshot', async () => {
-    nock('https://www.thecocktaildb.com').get(`/api/json/v1/1/list.php?c=list`).once().reply(200, {
+  beforeEach(() => {
+    nock('https://www.thecocktaildb.com').get(`/api/json/v1/1/list.php?c=list`).reply(200, {
       drinks: mockApiReturn,
     });
+  });
 
+  it('Should match snapshot', async () => {
     const { asFragment } = renderWithRouterAndQueryClient(<Categories />);
 
     await waitFor(() => {
       expect(screen.getByText('Beer')).toBeDefined();
       expect(screen.getByText('Cocoa')).toBeDefined();
-      expect(screen.getByText('Coffee')).toBeDefined();
+      expect(screen.getByText('Coffee / Tea')).toBeDefined();
     });
 
     expect(asFragment()).toMatchSnapshot();
   });
 
-  it.each(['Beer', 'Cocoa', 'Coffee'])('Should show %s', async (category) => {
+  it.each(['Beer', 'Cocoa', 'Coffee / Tea'])('Should show %s', async (category) => {
     renderWithRouterAndQueryClient(<Categories />);
+    await waitFor(() => {
+      expect(screen.getByText(category)).toBeDefined();
+    });
     expect(screen.getByText(category)).toBeDefined();
   });
 
   it('Should not show a category when it is not in the list', async () => {
     renderWithRouterAndQueryClient(<Categories />);
+    await waitFor(() => {
+      expect(screen.getByText('Beer')).toBeDefined();
+      expect(screen.getByText('Cocoa')).toBeDefined();
+      expect(screen.getByText('Coffee / Tea')).toBeDefined();
+    });
     expect(screen.queryByText('Ordinary drink')).toBeNull();
   });
 });
