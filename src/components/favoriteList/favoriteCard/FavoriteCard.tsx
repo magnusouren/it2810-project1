27429568bbf1bc25480ebe279226dae60fba8a0f
magnusouren <magnut0203@gmail.com>
2023-09-15 16:52:45 +0200

    add features to favoritecard

--- src/components/favoriteList/favoriteCard/FavoriteCard.tsx
+++ src/components/favoriteList/favoriteCard/FavoriteCard.tsx
@@ -4,19 +4,32 @@ import { FC } from 'react';
 import { Link } from 'react-router-dom';
 
 import { Drink } from '../../../types';
+import { toggleFavorite } from '../../../utils/persistency';
 
 interface FavoriteCardProps {
   drink: Drink;
 }
 
 export const FavoriteCard: FC<FavoriteCardProps> = ({ drink }) => {
+  const handleClick = () => {
+    toggleFavorite(drink.idDrink);
+  };
   return (
     <li key={drink.strDrink}>
       <img src={drink.strDrinkThumb} alt={drink.strDrink} />
-      <Link to={`/drink/${drink.idDrink}`}>
-        <h3>{drink.strDrink}</h3>
-      </Link>
-      <p className='material-symbols-outlined'>heart_minus</p>
+      <div>
+        <Link to={`/drink/${drink.idDrink}`}>
+          <h3>{drink.strDrink}</h3>
+        </Link>
+        <p className='extra-info'>
+          {drink.strCategory} - {drink.strGlass}
+        </p>
+      </div>
+      <button onClick={handleClick} className='remove-favorite'>
+        <span className='material-symbols-outlined' onClick={handleClick}>
+          heart_minus
+        </span>
+      </button>
     </li>
   );
 };
