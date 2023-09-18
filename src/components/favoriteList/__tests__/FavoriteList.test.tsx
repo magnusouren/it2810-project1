94ae209aaab0d1cb17de7e198c601273f2440622
magnusouren <magnut0203@gmail.com>
2023-09-18 20:22:23 +0200

    testing and documentation

---
+++ src/components/favoriteList/__tests__/FavoriteList.test.tsx
@@ -0,0 +1,59 @@
+import { screen, waitFor } from '@testing-library/react';
+import nock from 'nock';
+
+import { renderWithRouterAndQueryClient } from '../../../utils/test-utils';
+import { FavoriteList } from '../FavoriteList';
+import { testDrink, testDrink2 } from './mockDrinks';
+
+describe('FavoriteList', () => {
+  beforeAll(() => {
+    nock.disableNetConnect();
+  });
+
+  beforeEach(() => {
+    nock('https://www.thecocktaildb.com')
+      .get(`/api/json/v1/1/lookup.php?i=1`)
+      .reply(200, {
+        drinks: [testDrink],
+      });
+    nock('https://www.thecocktaildb.com')
+      .get(`/api/json/v1/1/lookup.php?i=2`)
+      .reply(200, {
+        drinks: [testDrink2],
+      });
+  });
+
+  afterAll(() => {
+    nock.cleanAll();
+    nock.enableNetConnect();
+  });
+
+  it('should render correctly when having 2 favorites', async () => {
+    const { container } = renderWithRouterAndQueryClient(<FavoriteList favorites={['1', '2']} />);
+    await waitFor(() => screen.getByText('Super Drink'));
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render correctly when having 1 favorite', async () => {
+    const { container } = renderWithRouterAndQueryClient(<FavoriteList favorites={['1']} />);
+    await waitFor(() => screen.getByText('Super Drink'));
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render correctly when having no favorite', async () => {
+    const { container } = renderWithRouterAndQueryClient(<FavoriteList favorites={[]} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should contain heading-element', async () => {
+    const { getByRole } = renderWithRouterAndQueryClient(<FavoriteList favorites={['1']} />);
+    await waitFor(() => screen.getByText('Super Drink'));
+    expect(getByRole('heading', { name: 'Your favorites:' })).toBeDefined();
+  });
+
+  it('should contain correct amount of favorite-cards', async () => {
+    const { getAllByRole } = renderWithRouterAndQueryClient(<FavoriteList favorites={['1', '2']} />);
+    await waitFor(() => screen.getByText('Super Drink'));
+    expect(getAllByRole('listitem').length).toBe(2);
+  });
+});
