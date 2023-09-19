4fef15966fcdcd17fd68f622c77ed3319b724441
Magnus Byrkjeland <magnus.e.b@hotmail.com>
2023-09-19 18:12:51 +0200

    test: inital tests

--- src/utils/__tests__/persistency.test.ts
+++ src/utils/__tests__/persistency.test.ts
@@ -1,4 +1,5 @@
-import { getFavorites, isFavorite, toggleFavorite } from '../persistency';
+import { DrinkOfTheDay } from '../../types';
+import { getDrinkOfTheDay, getFavorites, isFavorite, setDrinkOfTheDay, toggleFavorite } from '../persistency';
 
 describe('Persistency', () => {
   afterEach(() => {
@@ -26,5 +27,24 @@ describe('Persistency', () => {
     toggleFavorite('1');
     const favorite = isFavorite('1');
     expect(favorite).toEqual(true);
+  });
+
+  it('should return null if no drink of the day is set', () => {
+    const drink = getDrinkOfTheDay(new Date().toISOString().split('T')[0]);
+    expect(drink).toEqual(null);
+  });
+
+  it('should return the drink of the day', () => {
+    const date = new Date().toISOString().split('T')[0];
+    const drink: DrinkOfTheDay = {
+      drinkId: '1',
+      strDrink: 'Test Drink',
+      strDrinkThumb: 'test.jpg',
+      strCategory: 'category',
+      strAlcoholic: 'alcoholic',
+      strGlass: 'glass',
+    };
+    setDrinkOfTheDay(date, drink);
+    expect(getDrinkOfTheDay(date)).toEqual(drink);
   });
 });
