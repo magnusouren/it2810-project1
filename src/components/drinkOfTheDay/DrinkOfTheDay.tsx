2ac95242d49d169c6097e29e0aba693e8e645659
Ole Remi Dahl <olerd@stud.ntnu.no>
2023-09-19 18:13:40 +0200

    refactor fetchDrinkOfTheDay

--- src/components/drinkOfTheDay/DrinkOfTheDay.tsx
+++ src/components/drinkOfTheDay/DrinkOfTheDay.tsx
@@ -1,22 +1,19 @@
 import './DrinkOfTheDay.css';
 
 import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
-import { useState } from 'react';
+import { FC, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { DrinkOfTheDay as DrinkOfTheDayType } from '../../types';
 import { getDrinkOfTheDay } from '../../utils/persistency';
+import { fetchDrinkOfTheDay } from '../../utils/queries';
 
-export const DrinkOfTheDay: React.FC = () => {
+export const DrinkOfTheDay: FC = () => {
   // // Get the current date in YYYY-MM-DD format
   const currentDate = new Date().toISOString().split('T')[0];
 
-  // Check if a drink is stored for the current date in local storage
-  const storedDrink = getDrinkOfTheDay(currentDate);
-
   // Use the stored drink if available or an empty string if not
-  const [drink, setDrink] = useState<DrinkOfTheDayType | null>(storedDrink ? storedDrink : null);
+  const [drink, setDrink] = useState<DrinkOfTheDayType | null>(getDrinkOfTheDay(currentDate) || null);
 
   const { data, isLoading, isSuccess } = useQuery<DrinkOfTheDayType | null>(
     ['drinkOfTheDay'],
@@ -27,30 +24,10 @@ export const DrinkOfTheDay: React.FC = () => {
       }
 
       // Fetch a new random drink from the API
-      return axios
-        .get('https://www.thecocktaildb.com/api/json/v1/1/random.php')
-        .then((response) => {
-          const randomDrink = response.data.drinks[0];
-
-          const drink: DrinkOfTheDayType = {
-            drinkId: randomDrink.idDrink,
-            strDrink: randomDrink.strDrink,
-            strDrinkThumb: randomDrink.strDrinkThumb,
-            strCategory: randomDrink.strCategory,
-            strGlass: randomDrink.strGlass,
-            strAlcoholic: randomDrink.strAlcoholic,
-          };
-
-          // Store the new random drink ID in local storage with the current date
-          localStorage.setItem(currentDate, JSON.stringify(drink));
-
-          setDrink(drink);
-          return drink;
-        })
-        .catch((error) => {
-          console.error('Error fetching drink:', error);
-          return null;
-        });
+      return await fetchDrinkOfTheDay(currentDate).then((response) => {
+        setDrink(response);
+        return response;
+      });
     },
     {
       // Only refetch if the ID is empty (not stored in local storage)
