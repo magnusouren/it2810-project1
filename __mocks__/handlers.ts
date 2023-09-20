e4a4330d044bf7ed876b617ffdcad708b96a9d59
magnusouren <magnut0203@gmail.com>
2023-09-20 14:24:46 +0200

    return object as exported consts

--- __mocks__/handlers.ts
+++ __mocks__/handlers.ts
@@ -1,4 +1,6 @@
 import { rest } from 'msw';
+
+import { alternativeDrink, defaultDrinkResponse } from './mockObjects';
 export const handlers = [
   // Handles a POST /login request
 
@@ -35,22 +37,7 @@ export const handlers = [
       return res(
         ctx.status(200),
         ctx.json({
-          drinks: [
-            {
-              idDrink: '22222',
-              strDrink: 'Foo bar',
-              ingredients: [],
-              strGlass: 'Foo',
-              strInstructions: 'Instructions for foo drink',
-              strDrinkThumb: 'https://localhost:3000/drink/2.jpg',
-              strCategory: 'Category',
-              strAlcoholic: 'Alcoholic',
-              strIngredient1: 'Ingredient 1',
-              strMeasure1: 'Measure 1',
-              strIngredient2: 'Ingredient 2',
-              strMeasure2: 'Measure 2',
-            },
-          ],
+          drinks: [alternativeDrink],
         }),
       );
     }
@@ -58,32 +45,7 @@ export const handlers = [
     return res(
       ctx.status(200),
       ctx.json({
-        drinks: [
-          {
-            idDrink: '11118',
-            strDrink: 'Super Drink',
-            ingredients: [],
-            strGlass: 'Highball',
-            strInstructions: 'Instructions for drink',
-            strDrinkThumb: 'https://localhost:3000/drink/11118.jpg',
-            strCategory: 'Category',
-            strAlcoholic: 'Alcoholic',
-            strIngredient1: 'Ingredient 1',
-            strMeasure1: 'Measure 1',
-            strIngredient2: 'Ingredient 2',
-            strMeasure2: 'Measure 2',
-          },
-        ],
-      }),
-    );
-  }),
-
-  rest.get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php', (req, res, ctx) => {
-    req.url.searchParams.set('i', '0');
-    return res(
-      ctx.status(200),
-      ctx.json({
-        drinks: [],
+        drinks: [defaultDrinkResponse],
       }),
     );
   }),
