7cf8afe4d85b30e8b35a808a81037ab1048f663d
magnusouren <magnut0203@gmail.com>
2023-11-14 20:56:49 +0100

    persistent caching

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,28 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
+import { QueryClient } from '@tanstack/react-query';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
-const queryClient = new QueryClient({
+export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: Infinity,
+      staleTime: 1000 * 60 * 60 * 24, // 24 hours
     },
   },
+});
+
+const persister = createSyncStoragePersister({
+  storage: window.localStorage,
 });
 
 import { Router } from './routes';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <PersistQueryClientProvider client={queryClient} persistOptions={{ persister }}>
       <RouterProvider router={Router} />
-    </QueryClientProvider>
+    </PersistQueryClientProvider>
   </React.StrictMode>,
 );
--- src/pages/Drink.tsx
+++ src/pages/Drink.tsx
@@ -1,7 +1,10 @@
 import { FC } from 'react';
 
 import { DrinkCard } from '../components/drink/DrinkCard';
+import { queryClient } from '../main';
 
 export const Drink: FC = () => {
+  console.log(queryClient.getQueryCache());
+
   return <DrinkCard />;
 };
